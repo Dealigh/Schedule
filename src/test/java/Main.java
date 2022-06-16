f5045f6260e143fa446b15bb0bdff40e1444ea02
Leandro Moschner <l.espindolamoschner@gmail.com>
2022-06-16 16:19:45 -0300

    Some Logic Added. Look Out classes: Shifts, Days, Classrooms and Subject.

---
+++ src/test/java/Main.java
@@ -0,0 +1,12 @@
+import com.solvd.Schedule.binary.Student;
+import com.solvd.Schedule.dao.impl.StudentDAO;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Main {
+    private static final Logger LOG = LogManager.getLogger(com.solvd.Schedule.Main.class);
+
+    public static void main(String[] args) {
+
+    }
+}
